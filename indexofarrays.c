9edb90b5ddd6a405fcbb5e22be8c248bab2015b4
Bhumikareddynallamilli <96302064+Bhumikareddynallamilli@users.noreply.github.com>
2022-01-26 18:34:03 +0530

    Add files via upload

---
+++ indexofarrays.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int size;
+	scanf("%d",&size);
+	int num[size],i;
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&num[i]);
+	}
+	for(i=0; i<size; i++)
+	{
+		printf("index %d --> %d\n",i,num[i]);
+	}
+}
