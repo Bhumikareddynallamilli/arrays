2de34e999758920ae4d1ed85b030ec126e5ab99d
Bhumikareddynallamilli <96302064+Bhumikareddynallamilli@users.noreply.github.com>
2022-01-26 18:31:20 +0530

    Add files via upload

---
+++ evencountsusingarrays.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int size;
+	scanf("%d",&size);
+	int num[size],i;
+	for(i=0; i<size; i++)
+	{
+		scanf("%d",&num[i]);
+	}
+	int count=0;
+	for(i=0; i<size; i++)
+	{
+		if(num[i]%2 ==0)
+		{
+			count++;
+		}
+	}
+	printf("%d",count);
+}
