ca41efbf8ee337aeecfc7b43170edfc8d6200e44
Bhumikareddynallamilli <96302064+Bhumikareddynallamilli@users.noreply.github.com>
2022-02-25 22:08:08 +0530

    Add files via upload

---
+++ sumofdiagonalele.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+	int mat[10][10],row,col,i,j,sum=0;
+	scanf("%d %d",&row,&col);
+	for(i=0;i<row;i++)
+	{
+		for(j=0;j<col;j++)
+		{
+			scanf("%d",&mat[i][j]);
+		}
+	}
+	if(row==col)
+	{
+		for(i=0;i<row;i++)
+		{
+			for(j=0;j<col;j++)
+			{
+				if(i==j)
+				printf("%d",mat[i][j]);
+				else
+				printf("0");
+			}
+			printf("\n");
+		}
+	}
+	else
+	{
+		printf("impossible");
+	}
+}
