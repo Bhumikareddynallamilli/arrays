4f0e6b14aa2b1487949b598649e704e984d81420
Bhumikareddynallamilli <96302064+Bhumikareddynallamilli@users.noreply.github.com>
2022-02-25 23:05:28 +0530

    Add files via upload

---
+++ differenceoftwomartrix.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+int main()
+{
+	int a[2][2],b[2][2],c[2][2],i,j;
+	printf("enter 4 elements of matrix a\n");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("enter 4 elements of matrix b\n");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			scanf("%d",&b[i][j]);
+		}
+	}
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			c[i][j]=a[i][j]-b[i][j];
+		}
+	}
+	printf("matrix a=\n");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			printf("%d",a[i][j]);
+		}
+		printf("\n");
+	}
+	printf("matrix b=\n");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			printf("%d",b[i][j]);
+		}
+		printf("\n");
+	}
+	printf("difference of matrix a and b =\n");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			printf(" %d",c[i][j]);
+		}
+		printf("\n");
+	}
+}
