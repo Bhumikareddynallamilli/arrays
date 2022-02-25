6efcab0fd6cab656095da9dd56a2043036eb31c6
Bhumikareddynallamilli <96302064+Bhumikareddynallamilli@users.noreply.github.com>
2022-02-25 21:41:13 +0530

    Add files via upload

---
+++ sizeoflimits.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	printf("size of a character: %d\n",sizeof(char));
+	printf("size of a short integer: %d\n",sizeof(short int));
+	printf("size of a integer: %d\n",sizeof(int));
+	printf("size of a long integer:%d\n",sizeof(long int));
+	printf("size of a long long integer: %d\n",sizeof(long long int));
+	printf("size of a float:%d\n",sizeof(float));
+	printf("size of a double:%d\n",sizeof(double));
+}
